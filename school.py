460642d17860411dd1699bef046e8c1d7d916754
Sergey S <dest.studio@gmail.com>
2022-01-10 21:32:59 -0800

    done

--- school.py
+++ school.py
@@ -1,1 +1,26 @@
 class School:
+    
+    def __init__(self, name, roster ={}):
+        self.name = name
+        self.roster = roster
+    
+    def add_student(self, name, grade):
+        if str(grade) not in self.roster:
+            self.roster[str(grade)] = list()
+            self.roster[str(grade)].append(name)
+        else:
+            self.roster[str(grade)].append(name)
+    
+    def grade(self, grade):
+        return self.roster[str(grade)]
+    
+    def sort_roster(self):
+        for key, values in self.roster.items():
+            values_sorted = sorted(values, reverse= False)
+            self.roster[key] = values_sorted
+
+
+
+
+            
+
